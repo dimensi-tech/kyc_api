823a81e1576a32263bf559ebb7731183e7ccf350
Persada Ersad <ersad@doterb.com>
2019-10-09 22:40:25 +0700

    Configuration
    - Add Gemfile packages
    - Config global variable
    - Config rack cors

---
+++ config/initializers/global.rb
@@ -0,0 +1,5 @@
+Global.configure do |config|
+  config.environment = Rails.env.to_s
+  config.config_directory = Rails.root.join('config', 'global').to_s
+  config.yaml_whitelist_classes = [] # optional configuration
+end
