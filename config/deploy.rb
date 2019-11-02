d0d4ff6d71c203d6ead1a905b08c14238f29d647
Persada Ersad <ersad40@gmail.com>
2019-11-02 16:22:26 +0700

    Add Webpaker Config

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :branch, 'master'
 set :use_sudo, false
 append :linked_files, 'config/database.yml', 'config/secrets.yml', '.env'
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads'
-set :assets_roles, []
+
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
   task :make_dirs do
